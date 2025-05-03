bd6a07408ac8a128dbf8004daf4bdb412606b53a
Abijith Balaji <abijith.balaji@gmail.com>
2025-05-03 06:42:28 +0530

    Create  AltcoinContract.sol

---
+++ AltcoinContract.sol
@@ -0,0 +1,22 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+contract AltcoinContract {
+    string public name = "MyAltcoin";
+    string public symbol = "MAC";
+    uint8  public decimals = 18;
+    uint256 public totalSupply;
+    mapping(address => uint256) public balanceOf;
+
+    constructor(uint256 initialSupply) {
+        totalSupply = initialSupply * 10**decimals;
+        balanceOf[msg.sender] = totalSupply;
+    }
+
+    function transfer(address to, uint256 value) public returns (bool) {
+        require(balanceOf[msg.sender] >= value, "Low balance");
+        balanceOf[msg.sender] -= value;
+        balanceOf[to] += value;
+        return true;
+    }
+}
